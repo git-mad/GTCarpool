3a91d6d6f276d3fa6e2c8eb26ee160f1989966a0
kazilfr <kazilfr@gmail.com>
2024-09-23 15:16:43 -0400

    Add two blank fragments

--- app/src/main/java/com/example/gtcarpool/MainActivity.kt
+++ app/src/main/java/com/example/gtcarpool/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.gtcarpool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // setContentView(R.layout.bottom_nav) //  testing nav screen
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.bottom_nav) //  testing nav screen
+        val carpoolFragment = Carpoolfragment()
+        val messagesFragment = MessagesFragment()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setCurrentFragment(carpoolFragment)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(carpoolFragment)
+                R.id.carpool->setCurrentFragment(carpoolFragment)
+                R.id.settings->setCurrentFragment(carpoolFragment)
+                R.id.messages->setCurrentFragment(messagesFragment)
+            }
+            true
+        }
     }
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }

5c1cb999b77a10acfbb3c6d7eca06ec6ff2a347d
probablynotfiona <awesomeperson1004@gmail.com>
2024-10-06 11:21:11 -0400

    testing random thing

--- app/src/main/java/com/example/gtcarpool/ProfileFragment.kt
+++ app/src/main/java/com/example/gtcarpool/ProfileFragment.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-//import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -25,7 +25,7 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-   // private lateinit var auth: FirebaseAuth
+   private lateinit var auth: FirebaseAuth
     private lateinit var logoutButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +34,7 @@ class ProfileFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        //auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(

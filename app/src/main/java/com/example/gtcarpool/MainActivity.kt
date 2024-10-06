a7e8afabb8d338a66e93cdc68772a34e269c952d
NityaPotti <nityapotti01@gmail.com>
2024-10-05 21:57:55 -0400

    New Request

--- app/src/main/java/com/example/gtcarpool/MainActivity.kt
+++ app/src/main/java/com/example/gtcarpool/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.gtcarpool
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gtcarpool.NewRequest
+
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
@@ -11,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
         // Check if the user is logged in
@@ -28,12 +30,18 @@ class MainActivity : AppCompatActivity() {
             val profileFragment = ProfileFragment()
             val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
             setCurrentFragment(carpoolFragment)
+
             bottomNavigationView.setOnNavigationItemSelectedListener {
-                when(it.itemId){
+                when (it.itemId) {
                     R.id.home -> setCurrentFragment(carpoolFragment)
                     R.id.carpool -> setCurrentFragment(carpoolFragment)
                     R.id.settings -> setCurrentFragment(profileFragment)
                     R.id.messages -> setCurrentFragment(contactsFragment)
+                    R.id.add_request -> {
+                        // Navigate to NewRequest activity
+                        val intent = Intent(this, NewRequest::class.java)
+                        startActivity(intent)
+                    }
                 }
                 true
             }
